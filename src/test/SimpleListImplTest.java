98166831a4f4bfa8b84bd950df6c973d8e09b1c0
OmelchenkoE <lion_s.b.-18@mail.ru>
2018-11-15 12:25:38 +0200

    formatted file

--- src/test/SimpleListImplTest.java
+++ src/test/SimpleListImplTest.java
@@ -7,41 +7,43 @@ import static org.junit.Assert.*;
 
 
 public class SimpleListImplTest {
-SimpleListImpl target = new SimpleListImpl();
+    SimpleListImpl target = new SimpleListImpl();
     Object o = new Object();
 
     @Test
-    public void testGetAndSet(){
+    public void testGetAndSet() {
         target.add(o);
-        target.set(0,o);
-        assertEquals(o,target.get(0));
+        target.set(0, o);
+        assertEquals(o, target.get(0));
     }
 
     @Test
     public void testAddAndGet() {
-       target.add(o);
-       target.add(0,o);
-       assertEquals(o,target.get(0));
+        target.add(o);
+        target.add(0, o);
+        assertEquals(o, target.get(0));
     }
 
     @Test
     public void testRemove() {
-      target.add(o);
-      target.add("1");
-      target.remove(0);
-      target.remove("1");
-      assertTrue(target.isEmpty());
+        target.add(o);
+        target.add("1");
+        target.remove(0);
+        target.remove("1");
+        assertTrue(target.isEmpty());
     }
 
     @Test
     public void testSize() {
-        assertEquals(0,target.size());
+        assertEquals(0, target.size());
     }
 
     @Test
     public void testIsEmptyTrue() {
         assertTrue(target.isEmpty());
-    } @Test
+    }
+
+    @Test
     public void testIsEmptyFalse() {
         target.add(o);
         assertFalse(target.isEmpty());
