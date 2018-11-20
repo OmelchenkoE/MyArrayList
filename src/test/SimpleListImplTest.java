f549f7386a3dc2b7a8c57a285754cd9ef8975ba3
OmelchenkoE <lion_s.b.-18@mail.ru>
2018-11-20 18:21:33 +0200

    added new method and added new tests

--- src/test/SimpleListImplTest.java
+++ src/test/SimpleListImplTest.java
@@ -3,6 +3,8 @@ package test;
 import main.SimpleListImpl;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 
@@ -61,5 +63,58 @@ public class SimpleListImplTest {
         target.add(o);
         assertTrue(target.contains(o));
 
+    }
+
+    @Test
+    public void testToArray() {
+        Object[] testArr = new Object[16];
+        testArr[0] = o;
+        target.add(o);
+        assertEquals(testArr, target.toArray());
+    }
+
+    @Test
+    public void testContainsAll() {
+        target.add(o);
+        ArrayList list = new ArrayList();
+        list.add(o);
+        assertTrue(target.containsAll(list));
+    }
+
+    @Test
+    public void testAddAll() {
+        ArrayList list = new ArrayList();
+        list.add(o);
+        assertTrue(target.addAll(list));
+    }
+
+    @Test
+    public void testRemoveAll() {
+        target.add(o);
+        ArrayList list = new ArrayList();
+        list.add(o);
+        assertTrue(target.removeAll(list));
+
+    }
+
+    @Test
+    public void testRetainAll() {
+        ArrayList list = new ArrayList();
+        list.add(o);
+        list.add(1);
+        target.add(o);
+        assertTrue(target.retainAll(list));
+    }
+
+    @Test
+    public void testIndexOf() {
+        target.add(o);
+        assertEquals(0, target.indexOf(o));
+    }
+
+    @Test
+    public void testLastIndexOf() {
+        target.add(o);
+        assertEquals(0, target.indexOf(o));
     }
 }
