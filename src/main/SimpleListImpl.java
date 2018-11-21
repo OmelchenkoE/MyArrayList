fc646612ddbe6c014e45b7b53ab2e097860e8242
drykiss0 <antoni.rasul@gmail.com>
2018-11-21 12:06:12 +0100

    Review comments

--- src/main/SimpleListImpl.java
+++ src/main/SimpleListImpl.java
@@ -3,12 +3,17 @@ package main;
 import java.util.Collection;
 
 public class SimpleListImpl implements SimpleList {
-    private final int MIN_ARRAY_SIZE = 16;
+    private final int MIN_ARRAY_SIZE = 16;    
     private Object[] arr = new Object[MIN_ARRAY_SIZE];
+    // TODO
+    // General comment about the pointer:
+    // Seems that you don't fully understand what is contained
+    // in your array at the 'pointer' position
     private int pointer = 0;
 
     @Override
     public Object get(int index) {
+        // TODO do you need an "else" clause here?
         if (index < arr.length && index >= 0) {
             return arr[index];
         } else {
@@ -18,6 +23,7 @@ public class SimpleListImpl implements SimpleList {
 
     @Override
     public Object set(int index, Object element) {
+        // TODO: do you need an else clause here?
         if (index >= pointer && index < 0) {
             throw new IndexOutOfBoundsException();
         } else {
@@ -29,6 +35,7 @@ public class SimpleListImpl implements SimpleList {
 
     @Override
     public void add(int index, Object element) {
+        // TODO: do you need an else clause here?
         if (index > arr.length && index < 0) {
             throw new IndexOutOfBoundsException();
         } else {
@@ -46,6 +53,7 @@ public class SimpleListImpl implements SimpleList {
 
     @Override
     public Object remove(int index) {
+        // TODO: do you need an else clause here?
         if (index <= size() && index >= 0) {
             Object previousEl = arr[index];
             Object[] temp = arr;
@@ -75,6 +83,7 @@ public class SimpleListImpl implements SimpleList {
 
     @Override
     public boolean add(Object o) {
+        // TODO: this is incorrect - you still have 1 free space in array - but you're already resizing an array
         if (pointer == arr.length - 1) {
             Object[] temp = arr;
             arr = new Object[temp.length + MIN_ARRAY_SIZE];
@@ -90,6 +99,8 @@ public class SimpleListImpl implements SimpleList {
         for (int i = 0; i < pointer; i++) {
             if (o.equals(arr[i])) {
                 Object[] temp = arr;
+                // TODO: the following line is incorrect - your array will shrink every time you remove an element
+                // which makes the whole remove operation incorrect
                 if (size() <= (pointer - MIN_ARRAY_SIZE)) {
                     arr = new Object[arr.length - MIN_ARRAY_SIZE];
                 }
@@ -122,11 +133,16 @@ public class SimpleListImpl implements SimpleList {
 
     @Override
     public Object[] toArray() {
+        // Why didn't you use a simpler one-line function Arrays.copyOfRange(Object[] src, int from, int to)
         Object[]rArr = new Object[size()] ;
         System.arraycopy(arr, 0, rArr, 0, size());
         return rArr;
     }
 
+    // TODO In general, method will work, but:
+    // 1) Why do you call c.toArray() - it's a Collection, you can iterate over it directly
+    // 2) It is not efficient -  whenever any of elements from 'c' is not found - you can return 'false' immediately,
+    // no need to continue checks
     @Override
     public boolean containsAll(Collection<?> c) {
         int counter = 0;
@@ -139,6 +155,9 @@ public class SimpleListImpl implements SimpleList {
         return counter == c.size();
     }
 
+    // TODO - why do you count anything?
+    // You just need to add elements and return 'true'
+    // Your collection will ALWAYS change if you add elements
     @Override
     public boolean addAll(Collection<?> c) {
         int counter = 0;
@@ -150,6 +169,8 @@ public class SimpleListImpl implements SimpleList {
         return counter > 0;
     }
 
+    //TODO - you need a simple boolean flag for this method
+    //No need to count
     @Override
     public boolean removeAll(Collection<?> c) {
         int counter = 0;
@@ -161,6 +182,7 @@ public class SimpleListImpl implements SimpleList {
         return counter > 0;
     }
 
+    // TODO same here - you just need a boolean, no need to count
     @Override
     public boolean retainAll(Collection<?> c) {
         int counter = 0;
@@ -184,6 +206,8 @@ public class SimpleListImpl implements SimpleList {
     }
 
     @Override
+    // TODO: condition for the loop is incorrect (although it will probably run fine most of the time)
+    // Please check border conditions
     public int lastIndexOf(Object o) {
         for (int i = size(); i >= 0; i--) {
             if (o.equals(arr[i])) {
