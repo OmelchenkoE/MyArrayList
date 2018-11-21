5a20621dbe4750da81f6d8bfb915ed3588a34742
Rasul, Antoni <antoni.rasul@bottomline.com>
2018-11-21 11:15:33 +0100

    Initial Commit

---
+++ src/test/SimpleListImplTest.java
@@ -0,0 +1,125 @@
+package test;
+
+import main.SimpleListImpl;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class SimpleListImplTest {
+    SimpleListImpl target = new SimpleListImpl();
+    Object o = new Object();
+
+    @Test
+    public void testSet() {
+        target.add(o);
+        target.set(0, 1);
+        assertEquals(1, target.get(0));
+    }
+
+    @Test
+    public void testAddAndGet() {
+        for (int i = 0; i < 17; i++) {
+            target.add(o);
+        }
+        assertEquals(o, target.get(0));
+    }
+
+    @Test
+    public void testAddAndGetWithIndex() {
+        target.add(0, o);
+        assertEquals(o, target.get(0));
+    }
+
+    @Test
+    public void testRemove() {
+        target.add(o);
+        target.add("1");
+        target.remove(0);
+        target.remove("1");
+        assertTrue(target.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, target.size());
+    }
+
+    @Test
+    public void testIsEmptyTrue() {
+        assertTrue(target.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyFalse() {
+        target.add(o);
+        assertFalse(target.isEmpty());
+    }
+
+    @Test
+    public void testClear() {
+        target.add(o);
+        target.clear();
+        assertFalse(target.contains(o));
+    }
+
+    @Test
+    public void testContains() {
+        target.add(o);
+        assertTrue(target.contains(o));
+
+    }
+
+    @Test
+    public void testToArray() {
+        target.add(o);
+        assertEquals(o, target.toArray()[0]);
+    }
+
+    @Test
+    public void testContainsAll() {
+        target.add(o);
+        ArrayList list = new ArrayList();
+        list.add(o);
+        assertTrue(target.containsAll(list));
+    }
+
+    @Test
+    public void testAddAll() {
+        ArrayList list = new ArrayList();
+        list.add(o);
+        assertTrue(target.addAll(list));
+    }
+
+    @Test
+    public void testRemoveAll() {
+        target.add(o);
+        ArrayList list = new ArrayList();
+        list.add(o);
+        assertTrue(target.removeAll(list));
+
+    }
+
+    @Test
+    public void testRetainAll() {
+        ArrayList list = new ArrayList();
+        list.add(o);
+        list.add(1);
+        target.add(o);
+        assertTrue(target.retainAll(list));
+    }
+
+    @Test
+    public void testIndexOf() {
+        target.add(o);
+        assertEquals(0, target.indexOf(o));
+    }
+
+    @Test
+    public void testLastIndexOf() {
+        target.add(o);
+        assertEquals(0, target.lastIndexOf(o));
+    }
+}
